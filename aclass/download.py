81ed91216e517e7599afbfcc93e2e0902e7050d9
prybiec@tty420 <piotrrybiec28@gmail.com>
2021-01-12 22:15:50 +0100

    idk

--- aclass/download.py
+++ aclass/download.py
@@ -2,5 +2,15 @@
 import urllib.request
 import tqdm
 import platform
+from zipfile import ZipFile
 
 
+def download(url):
+    urllib.request.urlretrieve(url, '/mnt/c/Users/piotr/chromedriver_win32.zip')
+
+def download_webdriver(browser):
+  if browser == 'chrome88':
+    with ZipFile(download('https://chromedriver.storage.googleapis.com/88.0.4324.27/chromedriver_win32.zip'),'r') as obj:
+      obj.extractall()
+
+download_webdriver('chrome88')
