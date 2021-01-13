8ad4098abf6c4d6bb3c2e1550eb30c8cd5b1ac60
prybiec@tty420 <piotrrybiec28@gmail.com>
2021-01-13 15:48:42 +0100

    init requirements

--- aclass/__main__.py
+++ aclass/__main__.py
@@ -3,11 +3,14 @@
 import os
 import argparse
 
-parser = argparse.ArgumentParser(description='Automate you online classes')
-parser.add_argument('--configure', help='Add class URLs', action='store_true')
-
 def main():
-  pass
+  parser = argparse.ArgumentParser(description='Automate you online classes')
+  parser.add_argument('--configure', help='Add class URLs', action='store_true')
+  parser.add_argument('--join', help='Add class URLs', action='store_true', default=argparse.SUPRESS)
+  args = parser.parse_args()
+
+
+  print(args)
 
 if __name__ == "__main__":
   main()
