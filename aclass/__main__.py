3dfab75b0cca830fcc22ab4f07ce6ea5ab17b9f0
prybiec@tty420 <piotrrybiec28@gmail.com>
2021-01-13 16:36:38 +0100

    README update

--- aclass/__main__.py
+++ aclass/__main__.py
@@ -3,14 +3,19 @@
 import os
 import argparse
 
+
+#TODO: Make function that takes argument from args and joins the lesson
+def join():
+  pass
+
 def main():
   parser = argparse.ArgumentParser(description='Automate you online classes')
   parser.add_argument('--configure', help='Add class URLs', action='store_true')
-  parser.add_argument('--join', help='Add class URLs', action='store_true', default=argparse.SUPRESS)
+  parser.add_argument('--join', help='Add class URLs', default=argparse.SUPPRESS)
   args = parser.parse_args()
 
 
-  print(args)
+  print(args.join)
 
 if __name__ == "__main__":
   main()
