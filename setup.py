d27a86bc2317f4907a5aee9ce583100e02473495
prybiec@tty420 <piotrrybiec28@gmail.com>
2021-01-15 23:23:24 +0100

    version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(os.path.join(directory, 'README.md'), encoding='utf-8') as f:
 
 
 setup(name='aclass',
-      version='0.0.4',
+      version='0.0.5',
       description='Online Classes Automation',
       author='Piotrek Rybiec',
       license='MIT',
